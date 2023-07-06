d9a788d960b872c7d10de74fa99e2b72682157d8
Mike Rourke <mike.w.rourke@gmail.com>
2023-07-06 13:15:53 -0700

    feat: add additional exports

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,31 @@ export * from "@reduxjs/toolkit";
 
 export {
   default as createSagaMiddleware,
+  buffers,
+  END,
+  CANCEL,
+  channel,
+  detach,
   eventChannel,
+  multicastChannel,
+  runSaga,
+  stdChannel,
+  type Buffer,
+  type Channel,
+  type EffectMiddleware,
   type EventChannel,
+  type FlushableChannel,
+  type MulticastChannel,
+  type PredicateTakeableChannel,
+  type PuttableChannel,
+  type RunSagaOptions,
+  type Saga,
   type SagaIterator,
+  type SagaMiddleware,
+  type SagaMiddlewareOptions,
+  type SagaMonitor,
+  type TakeableChannel,
+  type Task,
 } from "redux-saga";
 
 export * from "redux-saga/effects";
