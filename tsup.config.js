abcf4165ab47654f66c73b493e8e5686a8cd0b36
Mike Rourke <mike.w.rourke@gmail.com>
2024-03-01 15:26:28 -0800

    chore: update `exports` field in `package.json` and `tsup` config file

--- tsup.config.js
+++ tsup.config.js
@@ -3,27 +3,25 @@ import { defineConfig } from "tsup";
 export default defineConfig(() => {
   const commonOptions = {
     clean: true,
-    sourcemap: true,
-    splitting: true,
+    // This is forwarded to Redux, just in case:
+    define: {
+      "process.env.NODE_ENV": JSON.stringify("production"),
+    },
+    entry: ["src/index.ts"],
     minify: false,
+    sourcemap: true,
     tsconfig: "./tsconfig.build.json",
   };
 
   return [
     {
       ...commonOptions,
-      entry: ["src/index.ts"],
-      // This is forwarded to Redux, just in case:
-      define: {
-        "process.env.NODE_ENV": JSON.stringify("production"),
-      },
-      format: "esm",
       dts: true,
+      format: "esm",
       outExtension: () => ({ js: ".mjs" }),
     },
     {
       ...commonOptions,
-      entry: ["src/index.ts"],
       format: "cjs",
       outExtension: () => ({ js: ".cjs" }),
     },
